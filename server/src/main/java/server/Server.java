016ac863bb16f8bda9f337966bfd6fb703184e5b
Junha <sjh666@byu.edu>
2024-04-05 11:02:47 -0600

    put a websocket path and websockethandler() into server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -3,9 +3,12 @@ package server;
 import com.google.gson.Gson;
 import dataAccess.DataAccessException;
 
+import server.webSocket.WebScoketHandler;
 import spark.*;  //导入Spark框架来创建java web 服务器
 
 public class Server {
+
+
 
     public int run(int desiredPort) { //启动服务器的方法 接受一个参数端口 80
         Spark.port(desiredPort);   // 将服务器端口设置为传入的参数 80
@@ -16,6 +19,12 @@ public class Server {
 
         // 使用spark.delete方法来request delete方式的http请求. 参数1是请求的url. db是clear的url. 然后使用匿名函数,
         // 传入spark里的request 和response 返回一个返回给用户的http RESPONSE. 一般是json的形式.
+
+        // 给webSocket的方法也在服务器里创造一个path和处理的接口 webScoketHandler 会处理前端发来的websocket请求 然后发送给service.
+        WebScoketHandler webScoketHandler = new WebScoketHandler();
+        Spark.webSocket("/connect", webScoketHandler);
+
+
         Spark.delete("/db", (request, response) -> new ClearOperation(request, response).clear());
         Spark.post("/user", (request, response) -> new AuthHandle(request, response).register());
         Spark.post("/session", (request, response) -> new AuthHandle(request, response).login());
