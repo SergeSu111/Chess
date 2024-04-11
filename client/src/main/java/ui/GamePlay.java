e6546c9bd2a1b543a1d75f88321be15ff1cb4d7b
Junha <sjh666@byu.edu>
2024-04-11 17:48:55 -0600

    finished the leave function in gamePlay but not finished makeMoves. I don know how to do it

--- client/src/main/java/ui/GamePlay.java
+++ client/src/main/java/ui/GamePlay.java
@@ -3,6 +3,7 @@ package ui;
 import chess.ChessBoard;
 import websocket.WebSocketFacade;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class GamePlay
@@ -22,8 +23,7 @@ public class GamePlay
         this.playerColor = playerColor;
     }
 
-    public void run()
-    {
+    public void run() throws ResponseException {
         System.out.println("Welcome to the game! Enjoy!");
         System.out.println("""
                 Help
@@ -50,10 +50,35 @@ public class GamePlay
         }
     }
 
-    public void help()
-    {
+    public void help() throws ResponseException {
         System.out.println("Choose one of the options to start:");
         run();
     }
+
+    public void reDrawBoard() throws ResponseException
+    {
+        BOARD.drawGeneralBoard(board, playerColor);
+        this.run();
+    }
+
+    public void leave() throws ResponseException, IOException {
+        System.out.println("You are leaving this game\n");
+        webSocketFacade.leave(serverFacade.authToken, gameID);
+        PostLogin postLogin = new PostLogin(scanner, serverFacade);
+        postLogin.help(); // 回到游戏前help的界面
+    }
+
+    // not finished
+    public void makeMoves()
+    {
+        System.out.print("Tell me your start position.");
+        int startPosition = scanner.nextInt();
+        System.out.print("Tell me where you want to go.");
+        int endPosition = scanner.nextInt();
+        System.out.print("Do you want to promote any piece? If so, enter what piece you want to promote: ");
+    }
+
+
+
 
 }
